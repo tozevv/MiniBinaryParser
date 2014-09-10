1503aa8d884ad9fd10e355f7268b1ddfad70da8e
António Vieira <antonio.vieira@bskyb.com>
2014-09-10 21:51:20 +0100

    Fixed path in release package

--- src/release-pack.bat
+++ src/release-pack.bat
@@ -1,4 +1,4 @@
-set NugetPath= ..\..\tools
+set NugetPath= ..\tools
 set MSBuildPath= C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 
 %MSBuildPath%\msbuild.exe MiniBinaryParser.sln /p:Configuration=Release /target:Clean,Build
