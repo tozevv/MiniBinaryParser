1e09b937868186b69e6fdcc14b0744ae53fd981c
António Vieira <antonio.vieira@bskyb.com>
2014-09-10 21:50:20 +0100

    Build package; move stuff around

---
+++ src/release-pack.bat
@@ -0,0 +1,6 @@
+set NugetPath= ..\..\tools
+set MSBuildPath= C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+%MSBuildPath%\msbuild.exe MiniBinaryParser.sln /p:Configuration=Release /target:Clean,Build
+%NugetPath%\nuget pack MiniBinaryParser.nuspec
+
